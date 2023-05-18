a96b0252e4dc2ef7ac8eed10c5eae23de1ed2165
KhayKhun <99352078+KhayKhun@users.noreply.github.com>
2023-05-18 09:17:03 +0630

    header font size

--- components/header.jsx
+++ components/header.jsx
@@ -4,7 +4,7 @@ import {FaGithub,FaFacebook,FaLinkedin} from 'react-icons/fa'
 
 const HeaderComponent = () => {
   return (
-    <header className='fixed top-0 w-screen bg-opacity-[0.5] z-[1000] flex px-[40px] py-[10px] lg:py-[20px] justify-between bg-teal-100 shadow-sm shadow-teal-300 text-sm sm:text-md lg:text-lg'>
+    <header className='fixed top-0 w-screen bg-opacity-[0.5] z-[1000] flex px-[40px] py-[10px] lg:py-[15px] justify-between bg-teal-100 shadow-sm shadow-teal-300 text-sm sm:text-md lg:text-lg'>
         <ul className='flex text-[16px] gap-[15px] font-bold'>
             <h1 className='text-black hover:cursor-default hidden sm:flex'>&lt;<span className='text-green-500'> simple </span>/&gt;</h1>
             <li><Link className='hover:scale-[1.3]' href="/">Home</Link></li>
--- components/projects.jsx
+++ components/projects.jsx
@@ -15,7 +15,7 @@ const ProjectsComponent = async () => {
     const data = await fetchPj();
 return (
     <div className='flex flex-col items-center'>
-      <h1 className='text-md md:text-2xl font-bold mb-[20px]'>Projects I have done so far ~</h1>
+      <h1 className='text-md md:text-2xl font-bold my-[20px]'>Projects I have done so far ~</h1>
       <ul className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[8px] w-full px-[40px]'>
         {
             data.map(d => <ProjectCard key={d.main.id} data={d}/>)
