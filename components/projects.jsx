8cf06b9c5cb032b9fa8c7c1975347a812595b8eb
KhayKhun <99352078+KhayKhun@users.noreply.github.com>
2023-05-18 07:54:32 +0630

    Update projects.jsx

--- components/projects.jsx
+++ components/projects.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import ProjectCard from './cards/projectCard';
 
-// async function fetchPj(){
-//     const response = await fetch('https://portfolio-nextjs-git-main-khaykhun.vercel.app/api/projects',{
-//       headers: {
-//         'Cache-Control': 'no-store',
-//       },
-//     });
-//     const data = await response.json();
-//     console.log(data);
-//     return data;
-// }
+async function fetchPj(){
+    const response = await fetch('https://portfolio-nextjs-khaykhun.vercel.app/api/projects',{
+      headers: {
+        'Cache-Control': 'no-store',
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
 
 const ProjectsComponent = async () => {
-    // const data = await fetchPj();
+    const data = await fetchPj();
 return (
     <div className='flex flex-col items-center'>
-      {/* <h1 className='font-3xl font-bold mb-[20px]'>Projects I have done so far ~</h1>
+      <h1 className='font-3xl font-bold mb-[20px]'>Projects I have done so far ~</h1>
       <ul className='grid grid-cols-4 gap-[8px] w-full px-[40px]'>
         {
             data.map(d => <ProjectCard key={d.main.id} data={d}/>)
         }
-      </ul> */}
+      </ul>
     </div>
   )
 }
