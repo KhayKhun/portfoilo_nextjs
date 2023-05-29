2cb15606c13774bac14cc16cdbaae1ea01102d3c
KhayKhun <99352078+KhayKhun@users.noreply.github.com>
2023-05-29 20:03:30 +0630

    google ana

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,6 @@ import './globals.css'
 import HeaderComponent from '@/components/header'
 import FooterComponent from '@/components/footer'
 import { Montserrat } from 'next/font/google'
-import Script from 'next/script'
 const MontserratFont = Montserrat({ subsets: ['latin'],weight : '400' })
 
 export const metadata = {
@@ -13,14 +12,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-QZKLNHKL35"></Script>
-      <Script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date());
-
-        gtag('config', 'G-QZKLNHKL35');
-      </Script>
       <body className={`${MontserratFont.className} min-h-screen bg-gradient-to-bl from-white to-gray-200`}>
         <HeaderComponent/>
         <main className=''>
--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,20 @@
-import dynamic from "next/dynamic"
 import WelcomeComponent from "@/components/welcome"
 import PersonalInformationComponent from "@/components/personal"
 import SkillsComponent from "@/components/skills"
+import Script from 'next/script'
 
 export default async function Home() {
 
   return (
   <div className="">
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-QZKLNHKL35"></Script>
+      <Script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
+
+        gtag('config', 'G-QZKLNHKL35');
+      </Script>
     <WelcomeComponent/>
     <PersonalInformationComponent/>
     <SkillsComponent/>
